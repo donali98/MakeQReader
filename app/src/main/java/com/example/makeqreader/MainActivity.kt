338f08f94e0a0017948d98c7d6bcfd18c6e9efd2
Dona <00084417@uca.edu.sv>
2020-07-15 15:27:39 -0600

    Side and bottom menu added to activity

--- app/src/main/java/com/example/makeqreader/MainActivity.kt
+++ app/src/main/java/com/example/makeqreader/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.makeqreader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Navigation methods
+        setSupportActionBar(toolbar)
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        setUpSideNavigationMenu(navController)
+        setUpActionBar(navController)
+        setUpBottomNavMenu(navController)
+    }
+
+    //Navigation method
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(Navigation.findNavController(this, R.id.nav_host_fragment), drawer_layout)
+    }
+
+    //Navigation method
+    private fun setUpSideNavigationMenu(navController: NavController){
+        nav_view!!.let {
+            NavigationUI.setupWithNavController(it, navController)
+        }
+    }
+    //Navigation method
+    private fun setUpBottomNavMenu(navController: NavController){
+        bottom_nav?.let {
+            NavigationUI.setupWithNavController(it, navController)
+        }
+    }
+    //Navigation method
+    private fun setUpActionBar(navController: NavController){
+        NavigationUI.setupActionBarWithNavController(this, navController, drawer_layout)
     }
 }
